2d5d9fa5d4f1ce4ac915d5e123ffaa485db6f445
Kimberly <kimberlybp19.18@ichat.sp.edu.sg>
2021-01-03 16:37:53 +0800

    added https filter, enable ssl

--- Task4/ProductStore/App_Start/WebApiConfig.cs
+++ Task4/ProductStore/App_Start/WebApiConfig.cs
@@ -19,6 +19,8 @@ namespace ProductStore
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Filters.Add(new ProductStore.Filters.RequireHttpsAttribute());
         }
     }
 }
