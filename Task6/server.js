d65d8fff76ff0dd5a361e30ec2460b6e12a85efc
Kimberly <kimberlybp19.18@ichat.sp.edu.sg>
2021-01-06 01:10:53 +0800

    added webhook endpoint, css and html changes, replaced subscription plan images

--- Task6/server.js
+++ Task6/server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const express = require('express');
 const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
 
 const envFilePath = path.resolve(__dirname, './.env');
 const env = require("dotenv").config({ path: envFilePath });
@@ -94,6 +95,39 @@ app.post('/customer-portal', async (req, res) => {
     url: portalsession.url,
   });
 });
+
+// Match the raw body to content type application/json
+app.post('/webhook', bodyParser.raw({type: 'application/json'}), (request, response) => {
+    let event;
+    console.log(request.body.type);
+  
+    try {
+      event = request.body;
+      console.log(event);
+    } catch (err) {
+      response.status(400).send(`Webhook Error: ${err.message}`);
+    }
+  
+    // Handle the event
+    switch (event.type) {
+      case 'payment_intent.succeeded':
+        const paymentIntent = event.data.object;
+        // Then define and call a method to handle the successful payment intent.
+        // handlePaymentIntentSucceeded(paymentIntent);
+        break;
+      case 'payment_method.attached':
+        const paymentMethod = event.data.object;
+        // Then define and call a method to handle the successful attachment of a PaymentMethod.
+        // handlePaymentMethodAttached(paymentMethod);
+        break;
+      // ... handle other event types
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+  
+    // Return a response to acknowledge receipt of the event
+    response.json({received: true});
+  });
 
 const server = http.createServer(app);
 const port = 3000;
