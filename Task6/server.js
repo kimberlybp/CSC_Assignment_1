a95c2b5e234ec152cd80596b703a5eb8f674b680
Kimberly <kimberlybp19.18@ichat.sp.edu.sg>
2021-01-05 23:18:03 +0800

    front-end html, css and javascript files for making payment for daily subscription, manage subscription redirect url also added, added addtional packages such as body-parser and dotenv

--- Task6/server.js
+++ Task6/server.js
@@ -2,25 +2,97 @@ const http = require('http');
 const express = require('express');
 const path = require('path');
 const app = express();
-const stripe = require('stripe')('sk_test_51I691rAolLg5hvOohzqUhTWOqXrXdNQZTvGBLPzMKNBvX5OA76LgtySfP8FKQbQHTWMoZBRfzTa6idf151fn1iBu00MhwEPQvu');
+
+const envFilePath = path.resolve(__dirname, './.env');
+const env = require("dotenv").config({ path: envFilePath });
+if (env.error) {
+  throw new Error(`Unable to load the .env file.`);
+}
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 app.use(express.json());
-app.use(express.static("express"));
+app.use(express.static("./src"));
 
+//Default URL 
+app.get('/', function (req, res) {
+    const filePath = path.resolve(__dirname + "/src/index.html");
+    res.sendFile(filePath);
+});
 
-// default URL for website
-app.get('/', function(req,res){
-    res.send('Hello World!')
+app.get("/setup", (req, res) => {
+    res.send({
+      publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+      starterPrice: process.env.STARTER_PRICE_ID,
+      basicPrice: process.env.BASIC_PRICE_ID,
+      premiumPrice: process.env.PREMIUM_PRICE_ID,
+    });
   });
 
-app.post('/customer-portal', async (req, res) => {
-    // Authenticate your user.
+  // Fetch the Checkout Session to display the JSON result on the success page
+app.get("/checkout-session", async (req, res) => {
+    const { sessionId } = req.query;
+    const session = await stripe.checkout.sessions.retrieve(sessionId);
+    res.send(session);
+  });
+  
+  app.post("/create-checkout-session", async (req, res) => {
+    const domainURL = process.env.DOMAIN;
+    const { priceId } = req.body;
+  
+    // Create new Checkout Session for the order
+    // Other optional params include:
+    // [billing_address_collection] - to display billing address details on the page
+    // [customer] - if you have an existing Stripe Customer ID
+    // [customer_email] - lets you prefill the email input in the form
+    // For full details see https://stripe.com/docs/api/checkout/sessions/create
+    try {
+      const session = await stripe.checkout.sessions.create({
+        customer: "cus_Ihc0JZ8uLAnsBk",
+        mode: "subscription",
+        payment_method_types: ["card"],
+        line_items: [
+          {
+            price: priceId,
+            quantity: 1,
+          },
+        ],
+        // ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
+        success_url: `${domainURL}/success.html?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${domainURL}/canceled.html`,
+      });
+  
+      res.send({
+        sessionId: session.id,
+      });
+    } catch (e) {
+      res.status(400);
+      return res.send({
+        error: {
+          message: e.message,
+        }
+      });
+    }
+  });  
 
-    const session = await stripe.billingPortal.sessions.create({
-        customer: 'cus_Ihc0JZ8uLAnsBk',
-        return_url: 'https://example.com/account',
-    });
-    
-    res.send(session.url);
+app.post('/customer-portal', async (req, res) => {
+   // For demonstration purposes, we're using the Checkout session to retrieve the customer ID. 
+  // Typically this is stored alongside the authenticated user in your database.
+  const { sessionId } = req.body;
+  const checkoutsession = await stripe.checkout.sessions.retrieve(sessionId);
+
+  // This is the url to which the customer will be redirected when they are done
+  // managing their billing with the portal.
+  const returnUrl = process.env.DOMAIN;
+
+  const portalsession = await stripe.billingPortal.sessions.create({
+    customer: checkoutsession.customer,
+    return_url: returnUrl,
+  });
+
+  res.send({
+    url: portalsession.url,
+  });
 });
 
 const server = http.createServer(app);
