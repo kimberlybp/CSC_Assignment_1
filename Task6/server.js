f7ffca32d50097dd4bb8f1a04407a58be7295f6d
Kimberly <kimberlybp19.18@ichat.sp.edu.sg>
2021-01-06 17:27:49 +0800

    implemented webhook to insert log to firebase db when subscription changes, success or failure charges

--- Task6/server.js
+++ Task6/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
+const firebase = require('firebase');
 
 const envFilePath = path.resolve(__dirname, './.env');
 const env = require("dotenv").config({ path: envFilePath });
@@ -12,7 +13,14 @@ if (env.error) {
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-app.use(express.json());
+app.use(bodyParser.json({
+  // Because Stripe needs the raw body, we compute it but only when hitting the Stripe callback URL.
+  verify: function(req,res,buf) {
+      var url = req.originalUrl;
+      if (url.startsWith('/webhook')) {
+          req.rawBody = buf.toString()
+      }
+  }}));
 app.use(express.static("./src"));
 
 //Default URL 
@@ -49,7 +57,6 @@ app.get("/checkout-session", async (req, res) => {
     // For full details see https://stripe.com/docs/api/checkout/sessions/create
     try {
       const session = await stripe.checkout.sessions.create({
-        customer: "cus_Ihc0JZ8uLAnsBk",
         mode: "subscription",
         payment_method_types: ["card"],
         line_items: [
@@ -97,37 +104,95 @@ app.post('/customer-portal', async (req, res) => {
 });
 
 // Match the raw body to content type application/json
-app.post('/webhook', bodyParser.raw({type: 'application/json'}), (request, response) => {
-    let event;
-    console.log(request.body.type);
-  
-    try {
-      event = request.body;
-      console.log(event);
-    } catch (err) {
-      response.status(400).send(`Webhook Error: ${err.message}`);
-    }
-  
+app.post('/webhook', bodyParser.raw({ type: '*/*' }), (request, response) => {
+  let event;
+
+  const signature = request.headers['stripe-signature'];
+
+  try {
+    event = stripe.webhooks.constructEvent(request.rawBody, signature, process.env.STRIPE_WEBHOOK_SECRET);
+
+    // https://stripe.com/docs/billing/subscriptions/overview#subscription-events
     // Handle the event
     switch (event.type) {
-      case 'payment_intent.succeeded':
-        const paymentIntent = event.data.object;
-        // Then define and call a method to handle the successful payment intent.
-        // handlePaymentIntentSucceeded(paymentIntent);
+      //any changes to subscription such as upgrading or donwgrading to another plan
+      case 'customer.subscription.updated':
+        sendToDB(event.id, event.data.object.id, event.type, createDateTimeString(event.created), event.created);
         break;
-      case 'payment_method.attached':
-        const paymentMethod = event.data.object;
-        // Then define and call a method to handle the successful attachment of a PaymentMethod.
-        // handlePaymentMethodAttached(paymentMethod);
+      //if customer cancels subscription
+      case 'customer.subscription.deleted':
+        sendToDB(event.id, event.data.object.id, event.type, createDateTimeString(event.created), event.created);
         break;
-      // ... handle other event types
+      //occurs during successful charges 
+      case 'invoice.paid':
+        if (event.data.object.billing_reason.includes('subscription')) {
+          sendToDB(event.id, event.data.object.id, event.type, createDateTimeString(event.created), event.created);
+        }
+        break;
+      //occurs during failed charges
+      case 'invoice.payment_failed':
+        console.log(event.data.object.billing_reason);
+        if (event.data.object.billing_reason.includes('subscription')) {
+          sendToDB(event.id, event.data.object.id, event.type, createDateTimeString(event.created), event.created);
+        }
+        break;
       default:
-        console.log(`Unhandled event type ${event.type}`);
+        //console.log(`Unhandled event type ${event.type}`);
     }
-  
+
     // Return a response to acknowledge receipt of the event
-    response.json({received: true});
-  });
+    response.json({ received: true });
+  }
+  catch (err) {
+    response.status(400).send(`Webhook Error: ${err.message}`);
+  }
+
+
+});
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyB7IaoQdnU5F80hh-JfyL548oRw3xB_ymk",
+    authDomain: "csc-task6.firebaseapp.com",
+    databaseURL: "https://csc-task6-default-rtdb.firebaseio.com",
+    projectId: "csc-task6",
+    storageBucket: "csc-task6.appspot.com",
+    messagingSenderId: "90231069859",
+    appId: "1:90231069859:web:4ddb58a8f9de85fa4cf316",
+    measurementId: "G-0RZMLN361J"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  let database = firebase.database();
+
+
+function sendToDB(eventid, id, type, datetime, timestamp) {
+    var obj = { 
+        eventid: eventid,
+        id: id,
+        type: type,
+        createdAt: datetime,
+        timestamp: timestamp
+    }
+
+    var oneRow = database.ref("subscription-log").child(obj.id);
+
+    oneRow.update (obj, (error) => {
+        if (error) {
+            // The write failed...
+            console.log("Failed with error: " + error);
+        } else {
+            // The write was successful...
+            console.log("success")
+        }
+    })
+
+ }
+
+ function createDateTimeString (timestamp){
+  var date = new Date(timestamp*1000)
+  var datetimeString = date.toLocaleDateString() + ' ' + date.toLocaleTimeString();
+  return datetimeString;
+ }
 
 const server = http.createServer(app);
 const port = 3000;
